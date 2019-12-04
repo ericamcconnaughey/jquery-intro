8e70d072759d548113c9fc736be64fa5207cf3b4
Erica McConnaughey <ericamcconnaughey@gmail.com>
2019-12-03 23:19:47 -0600

    add JS loop for clickable items and alerts

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,27 @@
 $(document).ready(function() {
-  $("h1").click(function() {
-    alert("This is a header.");
-  });
+  var clickableItems = [$("h1"), $("img")];
+  clickableItems.forEach(function(clickableItem) {
+    (clickableItem).click(function() {
+      alert("This is a(n) " + this.tagName + ".");
+  })
 
   $(".clickable").click(function() {
     $("#walrus-showing").slideToggle();
     $("#walrus-hidden").slideToggle();
   });
 
-  $("img").click(function() {
-    alert("This is an image.");
-  });
+/* OLD CODE BEFORE LOOPS 
+$("h1").click(function() {
+  alert("This is a header.");
 });
+
+$(".clickable").click(function() {
+  $("#walrus-showing").slideToggle();
+  $("#walrus-hidden").slideToggle();
+});
+
+$("img").click(function() {
+  alert("This is an image.");
+});*/
+})
+})
